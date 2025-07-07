4e4e42bede8e312e6ae52a24cc5136f986548b4c
Suvam Samantray <suvam.workforce@gmail.com>
2025-07-07 15:59:08 +0530

    image data extraction

--- extraction.py
+++ extraction.py
@@ -2,7 +2,8 @@ import pytesseract
 import cv2
 
 # import image
-image = cv2.imread('1.1.jpg')
+image = cv2.imread('img-01.1.jpg')
+# base_img = image.copy()
 
 # convertion to gray scale reduces complexity & noise, thus OCR works better
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -20,7 +21,7 @@ cv2.imwrite('temp/index_thresh.png', thresh)
 
 
 # kernel
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 5))
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
 cv2.imwrite('temp/index_kernel.png', kernel)
 
 
@@ -36,9 +37,25 @@ cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
 cnts = sorted(cnts, key=lambda x: cv2.boundingRect(x)[0])
 
+results = []
 for c in cnts:
     x, y, w,h = cv2.boundingRect(c)
     if h>200 and w>20:
+         roi = image[y:y+h, x:x+h]
          cv2.rectangle(image, (x, y), (x+w, y+h), (36, 255, 12), 2) 
+         ocr_result = pytesseract.image_to_string(roi)
+         ocr_result = ocr_result.split("\n")
+         for item in ocr_result:
+              results.append(item)
 
 cv2.imwrite('temp/index_bbox.png', image)
+
+# print (results)
+
+for item in results:
+     item = item.strip().replace('\n', '')
+     item = item.split(' ')[0]
+     
+     if len(item) > 0:
+          if item[0].isupper():
+               print(item)
